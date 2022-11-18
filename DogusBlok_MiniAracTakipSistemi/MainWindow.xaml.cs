16d0828de1461780b785afc9904da8db0c137a3f
mr-kaya <mustafa_kaya_09@hotmail.com>
2022-11-18 23:49:42 +0300

    MySQL Database Bağlandı.

--- DogusBlok_MiniAracTakipSistemi/MainWindow.xaml.cs
+++ DogusBlok_MiniAracTakipSistemi/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Data.Common;
+using MySql.Data.MySqlClient;
 using System.Data.OleDb;
 using System.Text.RegularExpressions;
 using System.Windows.Controls;
@@ -26,9 +27,9 @@ namespace DogusBlok_MiniAracTakipSistemi
 {
     public class Person
     {
-        public string İsim { get; set; }
-        public string Plaka { get; set; }
-        public string Giriş { get; set; }
+        public string? İsim { get; set; }
+        public string? Plaka { get; set; }
+        public string? Giriş { get; set; }
     }
     
     public partial class MainWindow : Window
@@ -52,7 +53,24 @@ namespace DogusBlok_MiniAracTakipSistemi
 
         private void ImportDatabaseToGridView()
         {
+            String mySqlConnectionString = "server=localhost;uid=root;database=dogusblok;";
+            MySqlConnection mySqlConn = new MySqlConnection(mySqlConnectionString);
+            mySqlConn.Open();
             
+            MySqlCommand selectTable = new MySqlCommand(@"SELECT * FROM person", mySqlConn);
+            MySqlDataReader myDataReader = selectTable.ExecuteReader();
+
+            List<Person> items = new List<Person>();
+            while (myDataReader.Read())
+            {
+                items.Add(new Person() {İsim = myDataReader[0].ToString(), Plaka = myDataReader[1].ToString(), Giriş = myDataReader[2].ToString()});
+            }
+            
+            MainListView.ItemsSource = items;
+            mySqlConn.Close();
+            
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(MainListView.ItemsSource);
+            view.Filter = UserFilterSearch;
         }
         
         private void ImportExcelToGridView(string FilePath)
@@ -64,33 +82,70 @@ namespace DogusBlok_MiniAracTakipSistemi
             DataTable dt = new DataTable();
             cmd_excel.Connection = con_excel;
             
-            con_excel.Open();
-            DataTable dataExcel = con_excel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
-            string sheetName = dataExcel.Rows[0]["TABLE_NAME"].ToString();
-
-            cmd_excel.CommandText = "Select * from [" + sheetName + "]";
-            ole_dataAdapter.SelectCommand = cmd_excel;
-            ole_dataAdapter.Fill(dt);
-
-            Debug.WriteLine($"Veritabanı veri sayısı : {dt.Rows.Count}");
-
+            String mySqlConnectionString = "server=localhost;uid=root;database=dogusblok;";
+            MySqlConnection mySqlConn = new MySqlConnection(mySqlConnectionString);
+            mySqlConn.Open();
             List<Person> items = new List<Person>();
-            for (int i = 0; i < dt.Rows.Count; i++)
-                items.Add(new Person() { İsim =  dt.Rows[i].Field<string>(0), Plaka = dt.Rows[i].Field<string>(1), Giriş = dt.Rows[i].Field<string>(2)});
-            
+
+            try
+            {
+                MySqlCommand Create_table =
+                    new MySqlCommand(
+                        @"CREATE TABLE person (name VARCHAR(100), licensePlate VARCHAR(20), entry VARCHAR(10));", mySqlConn);
+                Create_table.ExecuteNonQuery();
+
+                con_excel.Open();
+                DataTable dataExcel = con_excel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                string sheetName = dataExcel.Rows[0]["TABLE_NAME"].ToString();
+
+                cmd_excel.CommandText = "Select * from [" + sheetName + "]";
+                ole_dataAdapter.SelectCommand = cmd_excel;
+                ole_dataAdapter.Fill(dt);
+
+                Debug.WriteLine($"Veritabanı veri sayısı : {dt.Rows.Count}");
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    items.Add(new Person() { İsim =  dt.Rows[i].Field<string>(0), Plaka = dt.Rows[i].Field<string>(1), Giriş = dt.Rows[i].Field<string>(2)});
+                    MySqlCommand InsertInto =
+                        new MySqlCommand(
+                            @$"INSERT INTO person (name, licensePlate, entry) VALUES ('{dt.Rows[i].Field<string>(0)}','{dt.Rows[i].Field<string>(1)}','{dt.Rows[i].Field<string>(2)}');", mySqlConn);
+                    InsertInto.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Tablo Oluşturulmuş.");
+                MySqlCommand selectTable = new MySqlCommand(@"SELECT * FROM person", mySqlConn);
+                MySqlDataReader myDataReader = selectTable.ExecuteReader();
+
+                while (myDataReader.Read())
+                {
+                    items.Add(new Person() {İsim = myDataReader[0].ToString(), Plaka = myDataReader[1].ToString(), Giriş = myDataReader[2].ToString()});
+                }
+
+            }
             MainListView.ItemsSource = items;
             con_excel.Close();
+            mySqlConn.Close();
             
             CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(MainListView.ItemsSource);
-            view.Filter = UserFilter;
+            view.Filter = UserFilterSearch;
         }
         
         private void BtnSearch_OnClick(object sender, RoutedEventArgs e)
         {
-            CollectionViewSource.GetDefaultView(MainListView.ItemsSource).Refresh();
+            try
+            {
+                CollectionViewSource.GetDefaultView(MainListView.ItemsSource).Refresh();
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine($"ERR01: Uygun Kaynak Bulunamadı!!!");
+            }
         }
 
-        private bool UserFilter(object item)
+        private bool UserFilterSearch(object item)
         {
             if (String.IsNullOrEmpty(txtFilter.Text))
             {
@@ -104,7 +159,14 @@ namespace DogusBlok_MiniAracTakipSistemi
 
         private void TxtFilter_OnTextChanged(object sender, TextChangedEventArgs e)
         {
-            CollectionViewSource.GetDefaultView(MainListView.ItemsSource).Refresh();
+            try
+            {
+                CollectionViewSource.GetDefaultView(MainListView.ItemsSource).Refresh();
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine($"ERR01: Uygun Kaynak Bulunamadı!!!");
+            }
         }
     }
 }
